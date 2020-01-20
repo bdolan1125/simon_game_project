7361178b66faee66a788a33e460d882c1817e695
Benjamin Dolan <benjamin@thinkful.com>
2020-01-19 18:01:54 -0700

    added more js functionality onclick events

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
-
 let gamePattern = [];
+let userClickedPattern = [];
+
+$('.btn').click(function(){
+  let userChosenColour = $(this).attr('id'); 
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
 
 function nextSequence(){
   let randomNumber = Math.floor(Math.random()*4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  $('#'+ randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  $('#'+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
   let audio = new Audio('sounds/' + randomChosenColour + '.mp3');
   audio.play();
 }
+
+function animatePress(currentColour){
+  $('#' + currentColour).addClass('pressed');
+  setTimeout(function(){
+    $('#' + currentColour).removeClass('pressed');
+  }, 100);
+}
+
